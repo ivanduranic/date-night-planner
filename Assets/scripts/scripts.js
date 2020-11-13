636af19a6aa4b63da05740c1c559caa6b76943da
Dazrin Tioseco <xdazee@gmail.com>
2020-11-13 14:20:24 -0800

    added ability to get user location in mapbox

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -1,33 +1,22 @@
 
 
-function mapIt() {
-	var startPoint = sessionStorage.getItem("postalCode"); //user location
-  var destination = sessionStorage.getItem("yAddress");    //theatre location
-  var city = sessionStorage.getItem("city");               //get city location        
+//* Default Mapbox API Key
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGF6cmluIiwiYSI6ImNraGZia2txaDA2bDQyeG5tcnJuZ3VkMmUifQ.NUtgOZA8RiGDsB0evehxUg';
 
-  mapboxgl.accessToken =
-    "pk.eyJ1IjoiZGF6cmluIiwiYSI6ImNraGZibnRyZjA2aG0ycmw0OTlid2dzMWMifQ.c_2k33rh7-VmXsF2PbFbWg"; 
-  var map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11",
-    center: [-97.73675, 30.28608],
-    zoom: 13
-  });
-  map.on("load", function() {
-    var directions = new MapboxDirections({
-      accessToken: mapboxgl.accessToken,
-      // UI controls
-      controls: {
-        inputs: true,
-        instructions: false
-      }
-    });
-    map.addControl(directions, "top-left");
-
-    // Paramaters that pass starting point and Destination
-    directions.setOrigin(startPoint);
-    directions.setDestination(destination);
-    // Add zoom and rotation controls to the map.
-    map.addControl(new mapboxgl.NavigationControl());
-  });
-}
+//* Stores a mapbox object within a variable called map
+var map = new mapboxgl.Map({
+container: 'map', // container id
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [-96, 37.8], // starting position
+zoom: 3 // starting zoom
+});
+ 
+//* Add geolocate control to the map
+map.addControl(
+new mapboxgl.GeolocateControl({
+positionOptions: {
+enableHighAccuracy: true
+},
+trackUserLocation: true
+})
+);
