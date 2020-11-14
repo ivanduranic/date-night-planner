74ee48e2a31651430384b9f1c98b1062e90d4bfd
Mike TKachuk <tkachuk2314@gmail.com>
2020-11-14 00:15:05 -0500

    (pulling a string of theater names based on radius)

--- assets/scriptPage2-mike.js
+++ assets/scriptPage2-mike.js
@@ -1,37 +1,74 @@
-$(document).ready(function(){
-    $('.collapsible').collapsible();
-    $('.scrollspy').scrollSpy();
-    $('.carousel').carousel(
-        { padding: 0,
-            dist: 0,
-      
-      indicators: true,
-      duration: 100,
-    }
-    );
-    autoplay()   
-    function autoplay() {
-      $('.carousel').carousel('next');
-      setTimeout(autoplay, 4500);
-  };
-    
-        //     {
+$(document).ready(function () {
 
-// // CAROUSEL
-//     $('.carousel').carousel(
-//     {
-//       dist: 0,
-//       padding: 0,
-//       fullWidth: true,
-//       indicators: true,
-//       duration: 100,
-//     }
-//     );
-  
-  
-//   autoplay()   
-//   function autoplay() {
-//       $('.carousel').carousel('next');
-//       setTimeout(autoplay, 4500);
-//   };
+  // Components
+  $('.collapsible').collapsible();
+  $('.scrollspy').scrollSpy();
+  $('.carousel').carousel();
+
+  // Useful Functions
+
+
+
+  // Dom Selection
+  var movieChoice = document.getElementById(movieChoice);
+  var showTimes = document.getElementById(showTimes);
+
+
+  // Test
+  var getTheaterData = function () {
+    // Api components
+
+    // Start Date
+    var startDate = "startDate=2020-11-13";
+    // Radius and units of measurement mi=miles km=kilometers
+    var radius = "&radius=20";
+    var units = "&units=mi";
+    // Longitude & Lagitude
+    var lat = "&lat=43.592725";
+    var lng = "&lng=-79.542039";
+
+    // Api 
+    var apiUrl = "https:data.tmsapi.com/v1.1/movies/showings?" + startDate + lat + lng + radius + units + "&api_key=7dzhsf6hjxyc3vk9pabnbjmu";
+    console.log("http:data.tmsapi.com/v1.1/movies/showings?" + startDate + lat + lng + radius + units + "&api_key=ct3bfnvgcafwzwdvtp4khepg");
+
+    // Data Request
+    fetch(apiUrl)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+            console.log(data.length);
+
+            // Unique Theater List Array
+            var theaters = [];
+            for (var i = 0; i < data.length; i++) {
+              if (data[i].showtimes[i] !== undefined) {
+                theaters.push(data[i].showtimes[i].theatre.name);
+              };
+            };
+
+            // Filter Duplicate theater names (data is ordered movie's>higher order>theater)
+            var uniquetheaters = theaters.filter(function (i, index) {
+              return theaters.indexOf(i) === index;
+            });
+
+            console.log(uniquetheaters);
+
+            for ( var i = 0; i<uniquetheaters.length; i++){}
+            theaterChoice.innerHTML = uniquetheaters;
+          });
+
+
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to Theater API');
+      });
+  };
+
+  getTheaterData();
 });
+
+
