a97aa23ade049dfcbf9f528e7876b3d5b0f57eef
Ivan Duranic <ivan.duranic@gmail.com>
2020-11-14 15:58:18 -0500

    Created login process for our app

---
+++ Firebase login/main.js
@@ -0,0 +1,31 @@
+
+// ##########################################################
+// IMPORTANT:
+// This file could only go active if we have a Privacy Policy!
+// That's the rabbit hole we don't need to go into, right now!
+// ##########################################################
+
+
+// var mainApp = {};
+
+// (function() {
+//     var firebase = app_fireBase;
+// var uid = null;
+//     firebase.auth().onAuthStateChanged(function(user) {
+//         if (user) {
+//           // User is signed in.
+//           uid = user.id;
+//         }else {
+//             // redirect to login page
+//             uid = null;
+//             window.location.replace("login.html");
+//         }
+//       });
+
+//     function logOut() {
+//         firebase.auth().signOut();
+//     }
+
+//     mainApp.logOut = logOut;
+
+// })()
