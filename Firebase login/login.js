71edfd76bf3c4e5e05f8368695903b3e2e244792
Ivan Duranic <ivan.duranic@gmail.com>
2020-11-16 23:37:46 -0500

    login.html is ready to connect to input page that is index.html

--- Firebase login/login.js
+++ Firebase login/login.js
@@ -17,7 +17,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'main.html',
+    signInSuccessUrl: 'index.html',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
     //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -28,7 +28,7 @@ var uiConfig = {
       //firebase.auth.PhoneAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
-    tosUrl: 'main.html'
+    tosUrl: 'index.html'
     // Privacy policy url.
     //privacyPolicyUrl: '<your-privacy-policy-url>'
   };
